37542806f2b55b2745a8d83e3d1d37567520ca3e
L0G1C (David B) <dev@logiclodge.com>
2016-06-26 02:00:07 -0400

    create separate npm execution to separate out libraries needed for the app (lib-node), and those only needed during build time.  This means we don't have to include unneeded js files in the jar and cuts the jar size down by a third.  One day may switch over to use bower or another similar tool for this.

--- src/main/webapp/logiclodge-webapp/scripts/main.js
+++ src/main/webapp/logiclodge-webapp/scripts/main.js
@@ -1,10 +1,10 @@
 require.config({ 
 	paths : { 
-		jquery : '../node_modules/jquery/dist/jquery.min',
+		jquery : '../lib-node/node_modules/jquery/dist/jquery.min',
 		'jquery-ui' : '../lib/jquery-ui/jquery-ui.min',
-		lodash : '../node_modules/lodash/index',
-		backbone : '../node_modules/backbone/backbone-min',
-		text : '../node_modules/requirejs-text/text'
+		lodash : '../lib-node/node_modules/lodash/index',
+		backbone : '../lib-node/node_modules/backbone/backbone-min',
+		text : '../lib-node/node_modules/requirejs-text/text'
 	},
 	shim : {
 		backbone : {
